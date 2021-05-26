7ec374561ab855d9db9483c87779b22c8516d84a
Ivan Hitos <ivanhitos@gmail.com>
2021-05-26 15:38:30 +0100

    initial version

--- ec2-findhosts.py
+++ ec2-findhosts.py
@@ -1,3 +1,67 @@
 #!/usr/local/bin/python3
 #
-print("Placeholder")
+import argparse
+import logging
+import boto3
+import sys
+
+
+parser = argparse.ArgumentParser(description='Arguments ec2 find hosts script.')
+
+
+parser.add_argument(
+    '--region',
+    dest='region',
+    required=True,
+    help='Region to filter of'
+)
+
+parser.add_argument(
+    '--phase',
+    dest='phase',
+    required=True,
+    help='Phase to filter of'
+)
+
+parser.add_argument(
+    '--territory',
+    dest='territory',
+    default='*',
+    help='Territory to filter of'
+)
+
+
+parser.add_argument(
+    '--product',
+    dest='product',
+    default='*',
+    help='Product to filter of'
+)
+
+
+# logging class
+logging.basicConfig(stream=sys.stderr, level=logging.WARNING, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+# load arguments
+results = parser.parse_args()
+
+region=results.region
+phase=results.phase
+territory=results.territory
+product=results.product
+tagName=phase+"-"+territory+"-"+product+"-*"
+
+
+ec2 = boto3.client('ec2',region)
+reservations = ec2.describe_instances(
+    Filters=[ 
+            {'Name': 'tag:Name',
+             'Values': [tagName]}
+        ]
+    )
+
+for reservation in reservations["Reservations"] :
+    for instance in reservation["Instances"]:
+        for keyvalue in instance["Tags"]:
+            if keyvalue["Key"] == "Name":
+                print("%s \t-\t %s" % (instance['PrivateIpAddress'], keyvalue["Value"]))
